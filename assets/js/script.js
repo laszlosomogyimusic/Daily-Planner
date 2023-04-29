1a9f03b1ecd9b3b8ffc13a6589a4cd2185559c76
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-04-29 09:08:29 +0100

    Scheduler Times displayed The workday times are being displayed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
+var startHour = 9;
+var endHour = 17;
+
 function displayDate() {
   var currentDay = moment().format("dddd, MMMM Do");
   $("#currentDay").text(currentDay);
 }
 
+function displayScheduler() {
+  for(var i=startHour; i<=endHour; i++) {
+    var row = $("<div>");
+    row.addClass("row");
+    $(".container").append(row);
+    var hourField = $("<div>");
+    hourField.text(i);
+    hourField.addClass("col-md-2 hour");
+    row.append(hourField);
+  }
+}
+
 displayDate();
+displayScheduler();
