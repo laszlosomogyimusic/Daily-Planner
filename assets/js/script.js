e9b96c96aaf109a5023c64c00a4009a982d97915
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-04-30 06:49:29 +0100

    Text area displayed The text area is displayed for each hour.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,21 @@ function displayScheduler() {
     var row = $("<div>");
     row.addClass("row");
     $(".container").append(row);
-    var hourField = $("<div>");
-    hourField.text(i);
-    hourField.addClass("col-md-2 hour");
-    row.append(hourField);
+    var hourColumn = $("<div>");
+    var hourText = i + ":00";
+    hourColumn.text(hourText);
+    hourColumn.addClass("col-md-2 hour");
+
+    var descriptionColumn = $("<div>");
+    descriptionColumn.addClass("col-md-9 description p-0");
+    var textArea = $("<textarea>");
+    textArea.attr("id", i);
+    descriptionColumn.append(textArea);
+
+
+    row.append(hourColumn, descriptionColumn);
+
+
   }
 }
 
