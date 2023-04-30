1528b879a84cbe48e55dfd2fc970f61b9e82612e
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-04-30 07:27:45 +0100

    Adding IDs, CSS changes Each HTML tag in the generated list has a unique ID in CSS the textarea and saveBtn has 100% widht and height

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var startHour = 9;
+var startHour = 1;
 var endHour = 17;
 
 function displayDate() {
@@ -10,20 +10,41 @@ function displayScheduler() {
   for(var i=startHour; i<=endHour; i++) {
     var row = $("<div>");
     row.addClass("row");
-    $(".container").append(row);
+    row.attr("id", "row-" + i);
+    
+    //displaying the hour
     var hourColumn = $("<div>");
-    var hourText = i + ":00";
-    hourColumn.text(hourText);
     hourColumn.addClass("col-md-2 hour");
+    hourColumn.attr("id", "hour-col-" + i);
+    var hourText = $("<p>");
+    var displayedHour = i + ":00";
+    hourText.text(displayedHour);
+    hourColumn.append(hourText);
 
+    //displaying the input field area
     var descriptionColumn = $("<div>");
     descriptionColumn.addClass("col-md-9 description p-0");
+    descriptionColumn.attr("id", "description-col-" + i);
     var textArea = $("<textarea>");
     textArea.attr("id", i);
     descriptionColumn.append(textArea);
 
+    //displaying the dave button area
+    var saveButtonColumn = $("<div>");
+    saveButtonColumn.addClass("col-md-1");
+    saveButtonColumn.attr("id", "save-col-" + i);
 
-    row.append(hourColumn, descriptionColumn);
+    var saveButton = $("<button>");
+    saveButton.addClass("saveBtn");
+    saveButton.attr("id", "save-btn-" + i);
+    var saveIcon = $("<i>");
+    saveIcon.addClass("far fa-save fa-lg");
+    saveIcon.attr("id", "save-icon-" + i);
+    saveButton.append(saveIcon);
+    saveButtonColumn.append(saveButton);
+
+    row.append(hourColumn, descriptionColumn, saveButtonColumn);
+    $(".container").append(row);
 
 
   }
